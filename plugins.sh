cf7404108dd0146691e8d2f43aa1e9099dd110da
Georce <wujian@wujian360.cn>
2015-06-03 18:43:21 +0800

    Update plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -8,14 +8,16 @@
 # RUN /usr/local/bin/plugins.sh /plugins.txt
 #
 
+set -e
+
 REF=/usr/share/jenkins/ref/plugins
 mkdir -p $REF
 
-while read spec; do
+while read spec || [ -n "$spec" ]; do
     plugin=(${spec//:/ });
     [[ ${plugin[0]} =~ ^# ]] && continue
     [[ ${plugin[0]} =~ ^\s*$ ]] && continue
     [[ -z ${plugin[1]} ]] && plugin[1]="latest"
     echo "Downloading ${plugin[0]}:${plugin[1]}"
-    curl -s -L -f ${JENKINS_UC}/download/plugins/${plugin[0]}/${plugin[1]}/${plugin[0]}.hpi -o $REF/${plugin[0]}.hpi || echo "Failed to download ${plugin[0]}:${plugin[1]}"
+    curl -s -L -f ${JENKINS_UC}/download/plugins/${plugin[0]}/${plugin[1]}/${plugin[0]}.hpi -o $REF/${plugin[0]}.jpi
 done  < $1
